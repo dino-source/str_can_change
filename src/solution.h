f2612c4dd795c5763b71609ffeca1f6b8e03184b
Dino Source <dinosource.cpp@gmail.com>
2024-12-15 22:28:06 -0800

    Provide correct solution

--- src/solution.h
+++ src/solution.h
@@ -1,14 +1,24 @@
 #ifndef DINO_SOURCE_STR_CAN_CHANGE_H
 #define DINO_SOURCE_STR_CAN_CHANGE_H
 
-#include "btree.h"
 #include <string>
-#include <vector>
 
 class Solution {
 public:
     auto canChange(std::string const& s, std::string const& t) -> bool {
-        return {};
+        auto L{'L'}, R{'R'}, underscore{'_'};
+        for (auto n{std::ssize(s)}, i{0L}, j{0L}; i < n || j < n; ++i, ++j) {
+            while (i < n && s[i] == underscore) {
+                ++i;
+            }
+            while (j < n && t[j] == underscore) {
+                ++j;
+            }
+            if (s[i] != t[j] || (s[i] == L && i < j) || (s[i] == R && i > j)) {
+                return false;
+            }
+        }
+        return true;
     }
 };
 
--- test/str_can_change_test.cpp
+++ test/str_can_change_test.cpp
@@ -1,25 +1,20 @@
-#include <gtest/gtest.h>
 #include "../src/solution.h"
+#include <gtest/gtest.h>
 
 [[maybe_unused]] Solution sol;
 
 TEST(TestTopic, str_can_change_test_1) {
-    auto actual_result {1};
-    auto expected_result {1};
-
-    EXPECT_EQ(actual_result, expected_result);
+    EXPECT_TRUE(sol.canChange("_L__R__R_", "L______RR"));
 }
 
 TEST(TestTopic, str_can_change_test_2) {
-    auto actual_result {1};
-    auto expected_result {1};
-
-    EXPECT_EQ(actual_result, expected_result);
+    EXPECT_FALSE(sol.canChange("R_L_", "__LR"));
 }
 
 TEST(TestTopic, str_can_change_test_3) {
-    auto actual_result {1};
-    auto expected_result {1};
+    EXPECT_FALSE(sol.canChange("_R", "R_"));
+}
 
-    EXPECT_EQ(actual_result, expected_result);
+TEST(TestTopic, str_can_change_test_4) {
+    EXPECT_FALSE(sol.canChange("_LL__R__R_", "L___L___RR"));
 }
